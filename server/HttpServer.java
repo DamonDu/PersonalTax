2272b85e8bd759c8d387f7af829bf51ba1b7cafc
DamonDu <sy20030260@163.com>
2017-09-28 23:46:06 +0800

    fix path bug

--- server/HttpServer.java
+++ server/HttpServer.java
@@ -9,10 +9,7 @@ public class HttpServer {
 	/**
      * WEB_ROOT是HTML和其它文件存放的目录. 这里的WEB_ROOT为工作目录下的webroot目录
      */
-    public static final String WEB_ROOT = "F:\\eclipse-workspace\\PersonalTax\\resource";
-
-    // 关闭服务命令
-    private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+    public static final String WEB_ROOT = System.getProperty("user.dir") + "\\resource";
 
     public static void main(String[] args) {
         HttpServer server = new HttpServer();
@@ -34,25 +31,17 @@ public class HttpServer {
             InputStream input = null;
             OutputStream output = null;
             try {
-                //等待连接，连接成功后，返回一个Socket对象
                 socket = serverSocket.accept();
                 input = socket.getInputStream();
                 output = socket.getOutputStream();
 
-                // 创建Request对象并解析
                 Request request = new Request(input);
                 request.parse();
-                // 检查是否是关闭服务命令
-                /*if (request.getUri().equals(SHUTDOWN_COMMAND)) {
-                    break;
-                }*/
 
-                // 创建 Response 对象
                 Response response = new Response(output);
                 response.setRequest(request);
                 response.sendStaticResource();
 
-                // 关闭 socket 对象
                 socket.close();
                 
             } catch (Exception e) {
